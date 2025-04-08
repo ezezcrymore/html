7dd9d2bf335d3b71cf285684b8773d693c8bd13a
Yhwach <daniel.fernandes.santos19@escola.pr.gov.br>
2025-04-08 11:35:55 -0300

    Update script.js
    ez

---
+++ script.js
@@ -0,0 +1,9 @@
+function atualizarRelogio() {
+     const relogio = document.getElementById('relogio')
+    const agora = new Date();
+    agora.setHours(agora.getHours()-3)
+    cont horas = String(agora.getHours()).padStart(2,'0')
+    cont minutos = String(agora.getMinutes())pedStart(2,'0')
+    cont segundos = String(agora.getMinutes())pedStart(2,'0')
+    relogioElement.textContent = `${horas}` `${minutos}``${segundos}`
+}
