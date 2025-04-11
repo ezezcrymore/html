892092ad0d41a36763cd982b087178fab16d0cc7
Yhwach <daniel.fernandes.santos19@escola.pr.gov.br>
2025-04-11 09:33:49 -0300

    ez

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function hora() {
+
+    let relogio = document.getElementById('relogio')
+
+    let tempo = new Date();
+    let horas = tempo.getHours();
+    let minutos = tempo.getMinutes();
+    let segundos = tempo.getSeconds();
+    
+    relogio.innerHTML = `${horas}:${minutos}:${segundos}`
+ 
+}
+ 
+ hora();
 
